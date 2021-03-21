a6d77efb24af6671bd076882089f658932ae1297
hln9066@gmail.com <790799332op*>
2021-03-22 00:47:02 +0900

    revision

--- app/src/main/java/com/example/qrscanner/ErrorPage.java
+++ app/src/main/java/com/example/qrscanner/ErrorPage.java
@@ -1,5 +1,8 @@
 package com.example.qrscanner;
 
+import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
@@ -9,13 +12,58 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class ErrorPage extends AppCompatActivity {
+    Button btnORCam;
+    ListView dListView;
+    ArrayList<String> myArrayList;
 
+    SQLiteDatabase sqlDB;
+    Cursor cursor;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_error_page);
+        btnORCam=findViewById(R.id.btnORCam);
+        dListView =findViewById(R.id.lvDelivery);
+        myArrayList = new ArrayList<>();
+        final ArrayAdapter<String> myArrayAdapter = new ArrayAdapter<>(ErrorPage.this,
+                android.R.layout.simple_list_item_1, myArrayList);
+        dListView.setAdapter(myArrayAdapter);
+
+        //리스트뷰 보여주기
+        sqlDB=SQLiteDatabase.openDatabase("/data/data/com.example.qrscanner/databases/QRcodeDB",
+                null, SQLiteDatabase.OPEN_READONLY);
+        cursor = sqlDB.rawQuery("SELECT * FROM qrcodeTBL WHERE code LIKE 'e%';", null);
+        cursor.moveToFirst();
+        myArrayAdapter.notifyDataSetChanged();
+
+        do{
+            String code = "코드 : "+cursor.getString(0);
+            String address = "주소 : "+cursor.getString(8);
+            String note = "비고 : "+cursor.getString(10);
+
+            //Toast.makeText(DeliveryPage.this, ""+cursor.getPosition(), Toast.LENGTH_SHORT).show();
+            myArrayList.add(code+"\n"+ address+"\n"+note+"\n");
+            myArrayAdapter.notifyDataSetChanged();
+
+        }while(cursor.moveToNext());
+        cursor.close();
+        sqlDB.close();
+
+        btnORCam.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(ErrorPage.this, QRCamDComplete.class);
+                startActivity(intent);
+            }
+        });
 
     }
+
 }
--- app/src/main/java/com/example/qrscanner/ReturnPage.java
+++ app/src/main/java/com/example/qrscanner/ReturnPage.java
@@ -3,23 +3,62 @@ package com.example.qrscanner;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class ReturnPage extends AppCompatActivity {
-    Button btnRQRcamera;
+    Button btnORCam;
+    ListView dListView;
+    ArrayList<String> myArrayList;
+
+    SQLiteDatabase sqlDB;
+    Cursor cursor;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_return_page);
-        btnRQRcamera=findViewById(R.id.btnRQRcamera);
-        btnRQRcamera.setOnClickListener(new View.OnClickListener() {
+        btnORCam=findViewById(R.id.btnORCam);
+        dListView =findViewById(R.id.lvDelivery);
+        myArrayList = new ArrayList<>();
+        final ArrayAdapter<String> myArrayAdapter = new ArrayAdapter<>(ReturnPage.this,
+                android.R.layout.simple_list_item_1, myArrayList);
+        dListView.setAdapter(myArrayAdapter);
+
+        //리스트뷰 보여주기
+        sqlDB=SQLiteDatabase.openDatabase("/data/data/com.example.qrscanner/databases/QRcodeDB",
+                null, SQLiteDatabase.OPEN_READONLY);
+        cursor = sqlDB.rawQuery("SELECT * FROM qrcodeTBL WHERE code LIKE 'r%';", null);
+        cursor.moveToFirst();
+        myArrayAdapter.notifyDataSetChanged();
+
+        do{
+            String code = "코드 : "+cursor.getString(0);
+            String address = "주소 : "+cursor.getString(8);
+            String note = "비고 : "+cursor.getString(10);
+
+            //Toast.makeText(DeliveryPage.this, ""+cursor.getPosition(), Toast.LENGTH_SHORT).show();
+            myArrayList.add(code+"\n"+ address+"\n"+note+"\n");
+            myArrayAdapter.notifyDataSetChanged();
+
+        }while(cursor.moveToNext());
+        cursor.close();
+        sqlDB.close();
+
+        btnORCam.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent=new Intent(ReturnPage.this, QRCamDComplete.class);
                 startActivity(intent);
             }
         });
+
     }
+
 }
